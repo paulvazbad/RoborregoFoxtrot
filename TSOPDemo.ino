a0d88ca9ca5f9fe42df0a9111271b7859cf48fe7
Paul Vazquez <pev@live.com.mx>
2017-02-02 19:15:30 -0600

    Add files via upload

---
+++ TSOPDemo.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(15,INPUT);
+  Serial.begin(9600); 
+
+}
+
+void loop() {
+  int time = pulseIn(15,LOW);  
+  if(time){
+    Serial.println("Existe la pelota en mi direccion");
+    
+    }// put your main code here, to run repeatedly:
+
+}
