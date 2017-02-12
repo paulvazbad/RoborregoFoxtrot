6219ffb116853d031aae84e1dfcd7c10a61a961f
Paul Vazquez <pev@live.com.mx>
2017-02-11 19:24:38 -0600

    Add files via upload

---
+++ DriveMain.ino
@@ -0,0 +1,16 @@
+#include <Drive.h>
+#include <Wire.h>
+
+void setup() {
+  Serial.begin(9600);
+  Drive Mili(1,2,3,4);
+  Mili.Begin();
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  Mili.moveForward();
+  // put your main code here, to run repeatedly:
+
+}
