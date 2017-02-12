825f67476c6ae33102bc686ce42b84c86469857c
Paul Vazquez <pev@live.com.mx>
2017-02-12 13:44:28 -0600

    Update Drive.h

--- Drive.h
+++ Drive.h
@@ -1,16 +1,24 @@
-#ifndef Drive_h
+ifndef Drive_h
 #define Drive_h
 #include "Arduino.h"
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
 
 class Drive{
   public:
+    Drive();
+    Drive(int,int,int,int);
     void moveForward();
     void moveBackward();
     void Spin();
-
+    void Begin(int);
   private:
   int iSpeed;
+  Adafruit_MotorShield AdaShield = Adafruit_MotorShield();
+  Adafruit_DCMotor* IzquierdaDelante;
+  Adafruit_DCMotor* DerechaDelante;
+  Adafruit_DCMotor* IzquierdaAtras;
+  Adafruit_DCMotor* DerechaAtras;
 
  };
    #endif
-
