e93506f48fefe77839e5b7a778eba94c7bb95ada
Paul Vazquez <pev@live.com.mx>
2017-02-12 13:44:54 -0600

    Update Drive.cpp

--- Drive.cpp
+++ Drive.cpp
@@ -2,24 +2,24 @@
 #include "Drive.h"
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-
+#include "utility/Adafruit_MS_PWMServoDriver.h"
 Drive::Drive(){
-    Adafruit_MotorShield AdaShield = Adafruit_MotorShield();
-    Adafruit_DCMotor Motor1 = AdaShield.getMotor(1);
-    Adafruit_DCMotor Motor2 = AdaShield.getMotor(2);
-    Adafruit_DCMotor Motor3 = AdaShield.getMotor(3);
-    Adafruit_DCMotor Motor4 = AdaShield.getMotor(4);
+    
+    Adafruit_DCMotor* Motor1 = AdaShield.getMotor(1);
+    Adafruit_DCMotor* Motor2 = AdaShield.getMotor(2);
+    Adafruit_DCMotor* Motor3 = AdaShield.getMotor(3);
+    Adafruit_DCMotor* Motor4 = AdaShield.getMotor(4);
     }
 Drive :: Drive(int iPort1, int iPort2 , int iPort3, int iPort4){
-    Adafruit_MotorShield AdaShield = Adafruit_MotorShield();
-    Adafruit_DCMotor IzquierdaDelante = AdaShield.getMotor(iPort1);
-    Adafruit_DCMotor DerechaDelante = AdaShield.getMotor(iPort2);
-    Adafruit_DCMotor IzquierdaAtras = AdaShield.getMotor(iPort3);
-    Adafruit_DCMotor DerechaAtras = AdaShield.getMotor(iPort4);
+    AdaShield = Adafruit_MotorShield();
+    IzquierdaDelante = AdaShield.getMotor(iPort1);
+    DerechaDelante = AdaShield.getMotor(iPort2);
+    IzquierdaAtras = AdaShield.getMotor(iPort3);
+    DerechaAtras = AdaShield.getMotor(iPort4);
 
 
 }
-Drive:: Begin(int iDefaultSpeed){
+void Drive:: Begin(int iDefaultSpeed){
     AdaShield.begin();
     IzquierdaDelante->setSpeed(iDefaultSpeed);
     DerechaDelante->setSpeed(iDefaultSpeed);
@@ -28,13 +28,17 @@ Drive:: Begin(int iDefaultSpeed){
 
 }
 void Drive:: moveForward(){
-      //Velocidades para que avance hacia delante
+    IzquierdaDelante->run(FORWARD);
+    DerechaDelante->run(FORWARD);
+    IzquierdaAtras->run(FORWARD);
+    DerechaAtras->run(FORWARD);
+    //Velocidades para que avance hacia delante
 
     }
 void Drive:: moveBackward(){
+  //Velocidades para que retrocede
 }
 void Drive:: Spin(){
-
+//Velocidades para que gire a lo loco.
 
 }
-
