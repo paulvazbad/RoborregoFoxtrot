290ef24c58c6ef3cb5bfcab22f382f23f4fd8696
Paul Vazquez <pev@live.com.mx>
2017-02-17 16:20:59 -0600

    Update TSOPDemo.ino

--- TSOPDemo.ino
+++ TSOPDemo.ino
@@ -1,14 +1,41 @@
-void setup() {
-  pinMode(15,INPUT);
-  Serial.begin(9600); 
+#include <Arduino.h>
+
+int pinTSOP=8, iNoEncontrado=0;
+bool bSenal;
+void setup(){
+  pinMode(pinTSOP, INPUT);
+  pinMode(10,OUTPUT);
+  Serial.begin(9600);
+  digitalWrite(10, 1);
 
 }
 
-void loop() {
-  int time = pulseIn(15,LOW);  
-  if(time){
-    Serial.println("Existe la pelota en mi direccion");
-    
-    }// put your main code here, to run repeatedly:
+void loop(){
+  digitalWrite(10, 1);
+  bSenal = digitalRead(pinTSOP);
+  int bPulso;
+  bPulso = pulseIn(pinTSOP,LOW);
+  Serial.println("Valor: ");
+  Serial.println(bPulso);
 
+  if(bPulso>50&&bPulso<800){
+    Serial.println("Encontre la pelota");
+    iNoEncontrado=0;
+  }
+  else{
+    iNoEncontrado++;
+    Serial.println("No esta la pelota");
+    if(iNoEncontrado>20){
+      Serial.println(iNoEncontrado);
+      digitalWrite(10, 0);
+      iNoEncontrado=0;
+      Serial.println("Reiniciando sensor");
+      delay(3000);
+
+    }
+  }
+
+
+
+  delay(200);
 }
