f571c1983069189563d957408647fcaca4c5ae52
Paul Vazquez <pev@live.com.mx>
2017-02-23 00:11:13 -0600

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -1,3 +1,8 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
 #define qd A0
 #define qi A1
 #define qe A2
@@ -13,7 +18,7 @@ float DerD, IzqD, EnfrD, AtrasD;
 #define BackMotorLeftS2 9
 #define BackMotorRightS1 10
 #define BackMotorRightS2 11
-
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
 void setup() {
   Serial.begin(9600);
   //Qrd's
@@ -45,27 +50,99 @@ void moveForward(int tiempo){
   digitalWrite(BackMotorRightS2,HIGH);
   delay(tiempo);
 }
-void moveRight(int tiempo){
-  digitalWrite(FrontMotorLeftS1,HIGH);
-  digitalWrite(FrontMotorLeftS2,LOW);
-  digitalWrite(FrontMotorRightS1,HIGH);
-  digitalWrite(FrontMotorRightS2,LOW);
-  digitalWrite(BackMotorLeftS1,LOW);
-  digitalWrite(BackMotorLeftS2,HIGH);
-  digitalWrite(BackMotorRightS1,LOW);
-  digitalWrite(BackMotorRightS2,HIGH);  
-  delay(tiempo);
+void moveRight(int dOrientacionGrados){
+  sensors_event_t event;
+  bno.getEvent(&event);
+  Serial.print("X: ");
+  Serial.print(event.orientation.x, 4);
+  double dOrientacionAct=event.orientation.x;
+  double dOrientacionF=dOrientacionAct+dOrientacionGrados;
+  if(dOrientacionF<=360){
+    while(dOrientacionAct<=dOrientacionF){
+     dOrientacionAct=event.orientation.x;
+     digitalWrite(FrontMotorLeftS1,HIGH);
+     digitalWrite(FrontMotorLeftS2,LOW);
+     digitalWrite(FrontMotorRightS1,HIGH);
+     digitalWrite(FrontMotorRightS2,LOW);
+     digitalWrite(BackMotorLeftS1,LOW);
+     digitalWrite(BackMotorLeftS2,HIGH);
+     digitalWrite(BackMotorRightS1,LOW);
+     digitalWrite(BackMotorRightS2,HIGH);
+
+   }
+ }
+ else{
+   double dSobrante=dOrientacionF-360;
+   while(dOrientacionAct<=dOrientacionF){
+     dOrientacionAct=event.orientation.x;
+     digitalWrite(FrontMotorLeftS1,LOW);
+     digitalWrite(FrontMotorLeftS2,HIGH);
+     digitalWrite(FrontMotorRightS1,LOW);
+     digitalWrite(FrontMotorRightS2,HIGH);
+     digitalWrite(BackMotorLeftS1,HIGH);
+     digitalWrite(BackMotorLeftS2,LOW);
+     digitalWrite(BackMotorRightS1,HIGH);
+     digitalWrite(BackMotorRightS2,LOW);
+   }
+   while(dOrientacionAct<=dSobrante){
+     dOrientacionAct=event.orientation.x;
+     digitalWrite(FrontMotorLeftS1,LOW);
+     digitalWrite(FrontMotorLeftS2,HIGH);
+     digitalWrite(FrontMotorRightS1,LOW);
+     digitalWrite(FrontMotorRightS2,HIGH);
+     digitalWrite(BackMotorLeftS1,HIGH);
+     digitalWrite(BackMotorLeftS2,LOW);
+     digitalWrite(BackMotorRightS1,HIGH);
+     digitalWrite(BackMotorRightS2,LOW);
+   }
 }
-void moveLeft(int tiempo){
-  digitalWrite(FrontMotorLeftS1,LOW);
-  digitalWrite(FrontMotorLeftS2,HIGH);
-  digitalWrite(FrontMotorRightS1,LOW);
-  digitalWrite(FrontMotorRightS2,HIGH);
-  digitalWrite(BackMotorLeftS1,HIGH);
-  digitalWrite(BackMotorLeftS2,LOW);
-  digitalWrite(BackMotorRightS1,HIGH);
-  digitalWrite(BackMotorRightS2,LOW);  
-  delay(tiempo);
+}
+void moveLeft(int dOrientacionGrados){
+  //Falta revisar este bloque
+  sensors_event_t event;
+  bno.getEvent(&event);
+  Serial.print("X: ");
+  Serial.print(event.orientation.x, 4);
+  double dOrientacionAct=event.orientation.x;
+  double dOrientacionF=dOrientacionAct-dOrientacionGrados;
+  if(dOrientacionF>=0){
+    while(dOrientacionAct>dOrientacionF){
+       dOrientacionAct=event.orientation.x;
+       digitalWrite(FrontMotorLeftS1,LOW);
+       digitalWrite(FrontMotorLeftS2,HIGH);
+       digitalWrite(FrontMotorRightS1,LOW);
+       digitalWrite(FrontMotorRightS2,HIGH);
+       digitalWrite(BackMotorLeftS1,HIGH);
+       digitalWrite(BackMotorLeftS2,LOW);
+       digitalWrite(BackMotorRightS1,HIGH);
+       digitalWrite(BackMotorRightS2,LOW);
+     }
+  }
+  else{
+    double dSobrante=dOrientacionF+360;
+    while(dOrientacionAct>dOrientacionF){
+       dOrientacionAct=event.orientation.x;
+       digitalWrite(FrontMotorLeftS1,LOW);
+       digitalWrite(FrontMotorLeftS2,HIGH);
+       digitalWrite(FrontMotorRightS1,LOW);
+       digitalWrite(FrontMotorRightS2,HIGH);
+       digitalWrite(BackMotorLeftS1,HIGH);
+       digitalWrite(BackMotorLeftS2,LOW);
+       digitalWrite(BackMotorRightS1,HIGH);
+       digitalWrite(BackMotorRightS2,LOW);
+     }
+     while(dOrientacionAct>dSobrante){
+       dOrientacionAct=event.orientation.x;
+       digitalWrite(FrontMotorLeftS1,LOW);
+       digitalWrite(FrontMotorLeftS2,HIGH);
+       digitalWrite(FrontMotorRightS1,LOW);
+       digitalWrite(FrontMotorRightS2,HIGH);
+       digitalWrite(BackMotorLeftS1,HIGH);
+       digitalWrite(BackMotorLeftS2,LOW);
+       digitalWrite(BackMotorRightS1,HIGH);
+       digitalWrite(BackMotorRightS2,LOW);
+     }
+  }
   }
 void moveBackward(int tiempo){
   digitalWrite(FrontMotorLeftS1,LOW);
@@ -90,10 +167,8 @@ void Qrds(){
 
  /* Serial.print("El TCRT Atras: ");
   Serial.println(AtrasD);
-
   Serial.print("El TCRT Derecho: ");
   Serial.println(DerD);
-
   Serial.print("El TCRT Izquierdo: ");
   Serial.println(IzqD);
 */}
@@ -114,12 +189,14 @@ void Siguelineas(){
   }
   if(EnfrD==1){
     moveBackward(1500);
-  } 
+  }
 
 
 
 }
 void loop(){
+  moveForward(2000);
 Qrds();
 delay(500);
 }
+
